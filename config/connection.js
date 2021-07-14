d13dcd4423a969c9b742ca5a6bf76c33d0554b7c
camjofindlay <camjofindlay@gmail.com>
2021-07-13 20:17:05 -0400

    connection push

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,24 @@
-require("dotenv").config();
+const Sequelize = require('sequelize');
 
-const Sequelize = require("sequelize");
+require('dotenv').config();
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: "localhost",
-      dialect: "mysql",
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+const { DB_USER, DB_PW, DB_NAME } = process.env;
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    DB_NAME,
+    DB_USER,
+    DB_PW,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3302,
+    }
+  );
+}
 
 module.exports = sequelize;
